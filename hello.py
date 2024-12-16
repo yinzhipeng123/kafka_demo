63b99e57d12a864c293c09c3bbbb9beedb983f47
yinzhipeng123 <36987436+yinzhipeng123@users.noreply.github.com>
2024-12-16 12:25:19 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 #加个注释
 import time
-
+import datetime
 while True:
-    print("Hello")
+
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(current_time+" Hello")
     time.sleep(2)  # 每隔 2 秒输出一次 "Hello"
